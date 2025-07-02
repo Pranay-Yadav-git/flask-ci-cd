964a02fb1b532aced2c341828d647386ad5bda8e
PRANAY YADAV ALASANI <pranayalasani6@gmail.com>
2025-07-02 11:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "<h1>Welcome to My Dynamic Flask App</h1><p>Deployed using GitHub Actions + Docker!</p>"
+    return render_template('index.html', title='Home')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
